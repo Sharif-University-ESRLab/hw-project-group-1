87ddc67282fbabec34dd021baee235be0408a856
fatteme khashei <fatt3me@gmail.com>
2021-12-22 10:49:39 +0330

    change video_module

--- Code/Week 5/video_module.py
+++ Code/Week 5/video_module.py
@@ -1,45 +1,45 @@
 from picamera import PiCamera
 from time import sleep
 
+class Camera:
+    def __init__(self, resolution, framerate, brightness = 70):
+        self.base_url = '/home/pi/Desktop/camera_module'
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.camera.brightness = brightness
+        self.video_counter = 1
+        self.picture_counter = 1
+        self.calc_video_capacity()
 
-def config_camera(camera):
-    # camera.resolution = (1920, 1080)
-    # minimum 64 * 64
-    camera.resolution = (64, 64)
-    camera.framerate = 15
-    # between 0 and 100
-    camera.brightness = 70
+    def calc_video_capacity(self):
+        self.video_capacity = 10
 
-def init_camera(camera):
-    camera.start_preview(alpha = 200)
+    def start(self):
+        self.camera.start_preview(alpha = 200)
 
-def stop_camera(camera):
-    camera.stop_preview()
+    def stop(self):
+        self.camera.stop_preview()
 
-picture_counter = 1
-def capture_picture(camera):
-    camera.capture('/home/pi/Desktop/video_module/pictures/image_{picture_counter}.jpg')
-    picture_counter += 1
+    def capture_picture(self):
+        self.camera.capture('{self.base_url}/pictures/image_{self.picture_counter}.jpg')
+        self.picture_counter += 1
 
-video_counter = 1
-def start_recording(camera):
-    camera.start_recording('/home/pi/Desktop/video_module/video{video_counter}.h264')
-    video_counter += 1
+    def start_recording(self):
+        self.camera.start_recording('{self.base_url}/videdos/video{self.video_counter}.h264')
+        self.video_counter += 1
 
-def stop_recording(camera):
-    camera.stop_recording()
+    def stop_recording(self):
+        self.camera.stop_recording()
 
-max_capacity = 10
 runs = 0
-
-camera = PiCamera()
-config_camera(camera)
-init_camera(camera)
-while runs < max_capacity:
-    start_recording(camera)
+camera = Camera(resolution=(64, 64), framerate=15)
+camera.start()
+while runs < camera.video_capacity:
+    camera.start_recording()
     sleep(15)
-    capture_picture(camera)
+    camera.capture_picture()
     sleep(15)
-    stop_recording(camera)
+    camera.stop_recording()
     runs += 1
-stop_camera(camera)
+camera.stop()
