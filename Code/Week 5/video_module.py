fc1c821a11ce2d3db091c31c9e07fa1373929fb3
Parham Saremi <parhaamsaremi@gmail.com>
2021-12-22 12:16:28 +0330

    add calc_video_capacity

--- Code/Week 5/video_module.py
+++ Code/Week 5/video_module.py
@@ -2,21 +2,42 @@ from picamera import PiCamera
 from time import sleep
 from pathlib import Path
 import os
+import shutil
 
 class Camera:
-    def __init__(self, resolution, framerate, brightness = 70):
+    def __init__(self, resolution, framerate, brightness = 70, video_length = 30):
         self.base_url = '/home/pi/Desktop/camera_module'
         self.camera = PiCamera()
         self.camera.resolution = resolution
         self.camera.framerate = framerate
         self.camera.brightness = brightness
         self.video_counter = 1
+        self.video_length = video_length
         self.picture_counter = 1
-        self.calc_video_capacity()
+        self.video_capacity = self.calc_video_capacity()
 
     def calc_video_capacity(self):
-        self.video_capacity = 1
+        print(f"initializing video capacity calculation ...")
+        print("start recording dummy video for 5 seconds")
+        self.camera.start_recording(f'{self.base_url}/videos/dummy.h264')
+        sleep(2.5)
+        print("halfway there")
+        sleep(2.5)
+        self.camera.stop_recording()
 
+        video_size = os.path.getsize(f'{self.base_url}/videos/dummy.h264') * self.video_length/5
+        total_free_size = self.free_disk_size()
+        print("removing dummy file")
+        os.remove(f'{self.base_url}/videos/dummy.h264')
+        print(f"calculated -> possible video storage: {total_free_size/video_size} videos and {total_free_size/video_size*self.video_length} seconds")
+        return total_free_size/video_size, total_free_size/video_size*self.video_length
+        # returns count, total_time
+        
+
+
+
+
+        
     def check_video_capacity(self):
         directory = f'{self.base_url}/videos/'
         path = Path(directory)
@@ -44,6 +65,10 @@ class Camera:
 
     def stop_recording(self):
         self.camera.stop_recording()
+    
+    def free_disk_size(self):
+        total, used, free = shutil.disk_usage("/")
+        print("Free: %d GiB" % (free // (2**30)))
 
 runs = 0
 camera = Camera(resolution=(64, 64), framerate=15)
