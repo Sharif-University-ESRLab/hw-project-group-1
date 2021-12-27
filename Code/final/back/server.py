600bda4dec8badd17803bcd78c6993eeeb501a7f
Parham Saremi <parhaamsaremi@gmail.com>
2021-12-27 19:28:46 +0330

    base for recording

--- Code/final/back/server.py
+++ Code/final/back/server.py
@@ -5,7 +5,7 @@ with open(".token") as file:
     global_token = file.readline()
 
 alarm = 0
-
+recording = 0
 
 app = Flask(__name__)
 
@@ -13,8 +13,21 @@ app = Flask(__name__)
 def hello_world():
     return "welcome to control panel"
 
-@app.route("/login")
-def login():
-    return request.args.get("token")
+@app.route("/start_recording")
+def start_recording():
+    if request.args.get("token") != global_token:
+        return "invalid token"
 
+    return "temp"
 
+@app.route("/stop_recording")
+def stop_recording():
+    if request.args.get("token") != global_token:
+        return "invalid token"
+        
+    return "temp"
+
+@app.route("/status")
+def get_status():
+    if request.args.get("token") != global_token:
+        return "invalid token"
